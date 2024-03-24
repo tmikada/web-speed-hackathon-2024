066ce920949dd7c35485f74d4f7ef737b8e1d832
tmikada <29991871+tmikada@users.noreply.github.com>
2024-03-24 14:33:37 +0900

    メールアドレスとパスワードの正規表現を修正

--- workspaces/admin/src/pages/AuthPage/internal/LoginContent.tsx
+++ workspaces/admin/src/pages/AuthPage/internal/LoginContent.tsx
@@ -23,14 +23,16 @@ export const LoginContent: React.FC = () => {
         .required('メールアドレスを入力してください')
         .test({
           message: 'メールアドレスには @ を含めてください',
-          test: (v) => /^(?:[^@]*){12,}$/v.test(v) === false,
+          // test: (v) => /^(?:[^@]*){12,}$/v.test(v) === false,
+          test: (v) => /@/v.test(v),
         }),
       password: yup
         .string()
         .required('パスワードを入力してください')
         .test({
           message: 'パスワードには記号を含めてください',
-          test: (v) => /^(?:[^\P{Letter}&&\P{Number}]*){24,}$/v.test(v) === false,
+          // test: (v) => /^(?:[^\P{Letter}&&\P{Number}]*){24,}$/v.test(v) === false,
+          test: (v) => /[^\w\s]/.test(v),
         }),
     }),
   });
