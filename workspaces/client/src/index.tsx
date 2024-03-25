5a217600fb444aad2e33792f22890f033dd711bc
tmikada <29991871+tmikada@users.noreply.github.com>
2024-03-25 20:36:54 +0900

    jQuery消す

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,6 +1,5 @@
 // import './side-effects';
 
-import $ from 'jquery';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
@@ -8,19 +7,19 @@ import { SWRConfig } from 'swr';
 import { AdminApp } from '@wsh-2024/admin/src/index';
 import { ClientApp } from '@wsh-2024/app/src/index';
 
-import { preloadImages } from './utils/preloadImages';
+// import { preloadImages } from './utils/preloadImages';
 import { registerServiceWorker } from './utils/registerServiceWorker';
 
 const main = async () => {
-  await registerServiceWorker();
+  registerServiceWorker();
   // await preloadImages();
 
-  $(document).ready(() => {
+  document.addEventListener('DOMContentLoaded', () => {
     if (window.location.pathname.startsWith('/admin')) {
-      ReactDOM.createRoot($('#root').get(0)!).render(<AdminApp />);
+      ReactDOM.createRoot(document.getElementById('root')!).render(<AdminApp />);
     } else {
       ReactDOM.hydrateRoot(
-        $('#root').get(0)!,
+        document.getElementById('root')!,
         <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
           <BrowserRouter>
             <ClientApp />
