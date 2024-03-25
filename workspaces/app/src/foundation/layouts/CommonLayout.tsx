d9d75fa7ded7bc4749c4787b7689c970f3c82502
tmikada <29991871+tmikada@users.noreply.github.com>
2024-03-25 12:31:43 +0900

    Footerの横幅修正

--- workspaces/app/src/foundation/layouts/CommonLayout.tsx
+++ workspaces/app/src/foundation/layouts/CommonLayout.tsx
@@ -4,10 +4,23 @@ import styled from 'styled-components';
 import { Container } from '../components/Container';
 import { Footer } from '../components/Footer';
 import { Space } from '../styles/variables';
+import { BreakPoint, Color } from '../styles/variables';
 
 const _Content = styled.div`
   height: 100%;
   padding: 0 ${Space * 2}px;
+`;
+
+const _FooterContainer = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${BreakPoint.MOBILE}px;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: auto 1fr auto;
+  background-color: ${Color.MONO_A};
+  border-left: 1px solid ${Color.MONO_30};
+  border-right: 1px solid ${Color.MONO_30};
 `;
 
 export const CommonLayout: React.FC = () => {
@@ -18,7 +31,9 @@ export const CommonLayout: React.FC = () => {
           <Outlet />
         </_Content>
       </Container>
-      <Footer />
+      <_FooterContainer>
+        <Footer />
+      </_FooterContainer>
     </>
   );
 };
